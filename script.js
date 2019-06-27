1931e00a2cd2c3258809ef79eafc196a358543c5
eslam elaraby <islam.ismail038@gmail.com>
2019-06-27 18:11:35 +0200

    return longest words not just one

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
-function LongestWords(str) {
+//// this way to return just one word which is the longest word
+
+// function LongestWords(str) {
+//   let words = str.split(" ");
+//   let longestWord = "";
+//   for (let word of words) {
+//     if (word.length > longestWord.length) {
+//       longestWord = word;
+//     }
+//   }
+
+//   return longestWord;
+// }
+
+// console.log(LongestWords("i got up early today")); // early
+// console.log(LongestWords("i went straight to the beach")); // straight
+
+// the other way is to return the longest words
+
+function longestWords(str) {
   let words = str.split(" ");
-  let longestWord = "";
-  for (let word of words) {
-    if (word.length > longestWord.length) {
-      longestWord = word;
+  let size = 0;
+  let max = [""];
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length >= size) {
+      size = words[i].length;
+      if (max[max.length - 1].length < words[i].length) {
+        max = [];
+        max.push(words[i]);
+      } else {
+        max = [...max, words[i]];
+      }
     }
   }
-
-  return longestWord;
+  return [...max];
 }
 
-console.log(LongestWords("i got up early today"));
-console.log(LongestWords("i went straight to the beach"));
+console.log(longestWords("i got up early today")); // early
+console.log(longestWords("i went straight to the beach")); // straight
